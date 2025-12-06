eb08eeab0e6853c8b6fed8befc07996463e504d1
ludmattern <github.rfgrg@passmail.net>
2025-12-06 16:50:42 +0100

    feat: add CustomUser model with reputation property and permission checks for downvoting and deleting tips

--- Django - 3 - Sessions/d06/settings.py
+++ Django - 3 - Sessions/d06/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "ex.CustomUser"
+
 RANDOM_NAMES = [
     "Alice",
     "Bob",
--- Django - 3 - Sessions/ex/models.py
+++ Django - 3 - Sessions/ex/models.py
@@ -1,5 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
+
+
+class CustomUser(AbstractUser):
+    @property
+    def reputation(self):
+        rep = 0
+        for tip in self.tip_set.all():
+            rep += tip.upvotes.count() * 5
+            rep -= tip.downvotes.count() * 2
+        return rep
+
+    def has_perm(self, perm, obj=None):
+        if perm == "ex.downvote_tip":
+            return self.reputation >= 15
+        if perm == "ex.delete_tip":
+            return self.reputation >= 30
+        return super().has_perm(perm, obj)
 
 
 class Tip(models.Model):
