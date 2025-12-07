5ea90bde93d9ee20263f6b994fb1b1c86a80a1c8
ludmattern <github.rfgrg@passmail.net>
2025-12-07 15:53:10 +0100

    feat: streamline deployment script by removing Docker setup and updating step counts; remove PostgreSQL configuration from requirements

--- Django - 4 - Advanced/deploy.sh
+++ Django - 4 - Advanced/deploy.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-echo -e "[0/6] Recréation des containers existants..."
-docker compose down
-
-echo -e "[1/6] Démarrage de PostgreSQL..."
-docker compose up -d
-
-echo -e "[2/6] Création du virtualenv..."
+echo -e "[0/4] Création du virtualenv..."
 python3 -m venv .venv
 
-echo -e "[3/6] Installation des dépendances..."
+echo -e "[1/4] Installation des dépendances..."
 source .venv/bin/activate
 pip install -r requirements.txt
 
-echo -e "[4/6] Vérification de la connexion..."
+echo -e "[2/4] Vérification de la connexion..."
 python manage.py check
 
-echo -e "[5/6] Application des migrations..."
+echo -e "[3/4] Application des migrations..."
 python manage.py migrate
 
-echo -e "[6/6] Lancement du serveur..."
+echo -e "[4/4] Lancement du serveur..."
 python3 manage.py runserver
